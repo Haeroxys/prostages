c5d1d35723c4b9d6726a2a9575539178150a68c0
Arthur Shovel Knight <achauvel003@iutbayonne.univ-pau.fr>
2021-11-30 17:55:31 +0100

    TP2

--- src/Controller/OpenclassdutController.php
+++ src/Controller/OpenclassdutController.php
@@ -9,12 +9,43 @@ use Symfony\Component\Routing\Annotation\Route;
 class OpenclassdutController extends AbstractController
 {
     /**
-     * @Route("/openclassdut", name="openclassdut")
+     * @Route("/", name="accueil")
      */
-    public function index(): Response
+    public function accueil(): Response
     {
-        return $this->render('openclassdut/index.html.twig', [
+        return $this->render('openclassdut/accueil.html.twig', [
             'controller_name' => 'OpenclassdutController',
+        ]);
+    }
+
+    /**
+     * @Route("/entreprises", name="entreprises")
+     */
+    public function entreprises(): Response
+    {
+        return $this->render('openclassdut/entreprises.html.twig', [
+            'controller_name' => 'OpenclassdutController',
+        ]);
+    }
+
+    /**
+     * @Route("/formations", name="formations")
+     */
+    public function formations(): Response
+    {
+        return $this->render('openclassdut/formations.html.twig', [
+            'controller_name' => 'OpenclassdutController',
+        ]);
+    }
+
+    /**
+     * @Route("/stages/{id}", name="stages")
+     */
+    public function stages($id): Response
+    {
+        return $this->render('openclassdut/stages.html.twig', [
+            'controller_name' => 'OpenclassdutController',
+            'id' => $id,
         ]);
     }
 }
