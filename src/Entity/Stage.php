e39ba4f8646917226786b03f92da50a1006a60e9
Arthur Shovel Knight <achauvel003@iutbayonne.univ-pau.fr>
2022-01-06 10:58:10 +0100

    création des entités de la base de données

---
+++ src/Entity/Stage.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StageRepository::class)
+ */
+class Stage
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $descMission;
+
+    /**
+     * @ORM\Column(type="string", length=30)
+     */
+    private $emailContact;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
+
+    public function setCode(int $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescMission(): ?string
+    {
+        return $this->descMission;
+    }
+
+    public function setDescMission(string $descMission): self
+    {
+        $this->descMission = $descMission;
+
+        return $this;
+    }
+
+    public function getEmailContact(): ?string
+    {
+        return $this->emailContact;
+    }
+
+    public function setEmailContact(string $emailContact): self
+    {
+        $this->emailContact = $emailContact;
+
+        return $this;
+    }
+}
