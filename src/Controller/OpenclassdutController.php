2e7c0fa2b3740f7e753c0f8de839d12fe747c57f
Arthur Shovel Knight <achauvel003@iutbayonne.univ-pau.fr>
2022-01-06 09:54:08 +0100

    renommage de OpenclassDut en Prostages

--- src/Controller/OpenclassdutController.php
+++ src/Controller/OpenclassdutController.php
@@ -6,45 +6,45 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class OpenclassdutController extends AbstractController
+class ProstagesController extends AbstractController
 {
     /**
-     * @Route("/", name="openclassdut_accueil")
+     * @Route("/", name="prostages_accueil")
      */
     public function accueil(): Response
     {
-        return $this->render('openclassdut/accueil.html.twig', [
-            'controller_name' => 'OpenclassdutController',
+        return $this->render('prostages/accueil.html.twig', [
+            'controller_name' => 'ProstagesController',
         ]);
     }
 
     /**
-     * @Route("/entreprises", name="openclassdut_entreprises")
+     * @Route("/entreprises", name="prostages_entreprises")
      */
     public function entreprises(): Response
     {
-        return $this->render('openclassdut/entreprises.html.twig', [
-            'controller_name' => 'OpenclassdutController',
+        return $this->render('prostages/entreprises.html.twig', [
+            'controller_name' => 'ProstagesController',
         ]);
     }
 
     /**
-     * @Route("/formations", name="openclassdut_formations")
+     * @Route("/formations", name="prostages_formations")
      */
     public function formations(): Response
     {
-        return $this->render('openclassdut/formations.html.twig', [
-            'controller_name' => 'OpenclassdutController',
+        return $this->render('prostages/formations.html.twig', [
+            'controller_name' => 'ProstagesController',
         ]);
     }
 
     /**
-     * @Route("/stages/{id}", name="openclassdut_stages")
+     * @Route("/stages/{id}", name="prostages_stages")
      */
     public function stages($id): Response
     {
-        return $this->render('openclassdut/stages.html.twig', [
-            'controller_name' => 'OpenclassdutController',
+        return $this->render('prostages/stages.html.twig', [
+            'controller_name' => 'ProstagesController',
             'idStage' => $id,
         ]);
     }
