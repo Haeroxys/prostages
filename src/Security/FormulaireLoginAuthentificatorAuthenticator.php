9285e11b206b83b7e527f70acaabfa7a7cfe0d51
Arthur Shovel Knight <achauvel003@iutbayonne.univ-pau.fr>
2022-03-21 14:43:05 +0100

    fin de l'implémentation d'un formulaire de connexion

--- src/Security/FormulaireLoginAuthentificatorAuthenticator.php
+++ src/Security/FormulaireLoginAuthentificatorAuthenticator.php
@@ -96,7 +96,7 @@ class FormulaireLoginAuthentificatorAuthenticator extends AbstractFormLoginAuthe
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('prostages_stages'));
     }
 
     protected function getLoginUrl()
